816f82ade7b5bcc9c8abef75d8e00222dccd8ec2
Savidya Godamune <savi.godamune@gmail.com>
2026-07-04 12:33:41 +0530

    Merge branch 'savi'

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
